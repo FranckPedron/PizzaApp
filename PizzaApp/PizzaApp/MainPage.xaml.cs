c4535de0c5ae4199f4efd47647d83a39cf4933cf
franckyp1 <franckyp1+rapports_git@gmail.com>
2020-05-29 22:10:08 +0200

    Première version

---
+++ PizzaApp/PizzaApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PizzaApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PizzaApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<Pizza> pizzas;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            pizzas = new List<Pizza>();
+            pizzas.Add(new Pizza { nom = "Végétarienne", prix = 7, ingredients = new string[] { "Tomate", "poivrons", "oignons" } });
+            pizzas.Add(new Pizza { nom = "Montagnarde", prix = 11, ingredients = new string[] { "Reblochon", "pommes de terre", "oignons","crème" } });
+            pizzas.Add(new Pizza { nom = "Carnivore", prix = 14, ingredients = new string[] { "Tomate", "viande hachée", "Mozzarella" } });
+
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/Model/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.Model
+{
+    public class Pizza
+    {
+        public string nom { get; set; }
+        public int prix { get; set; }
+        public string[] ingredients { get; set; }
+
+        public Pizza()
+        {
+        }
+
+    }
+}
